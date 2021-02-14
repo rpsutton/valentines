5d262572006f7f45adb62e69846982ec6eb0e8cc
rpsutton <rpsiii99@gmail.com>
2021-02-13 23:04:29 -0500

    background change

--- src/components/DisplaySection.js
+++ src/components/DisplaySection.js
@@ -18,7 +18,7 @@ function DisplaySection(props) {
     <section
         style={{height: props.height}}
         className={
-        "gradient SectionComponent py-5 position-relative d-flex justify-content-center align-items-center" +
+        "bg-pink SectionComponent py-5 position-relative d-flex justify-content-center align-items-center" +
         (props.textColor ? ` text-${props.textColor}` : "") +
         (className ? ` ${className}` : "")}
       {...otherProps}
